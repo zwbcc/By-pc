b2c0465c9aed81f597200ebd4aa7d30ec497e1bb
zwbcc <1095561636@qq.com>
2018-12-20 16:52:56 +0800

    判断终端

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
       }
     },
 　　_isMobile() {
-      let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+      const flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
       return flag;
     }
   },
